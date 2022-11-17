dc406dd701b91b2b612db23d83f235dd68ef4a03
noman318 <shaikhmohdnoman612@gmail.com>
2022-11-17 18:30:10 +0530

    Minor fixes and scope error fixed

--- backend/index.js
+++ backend/index.js
@@ -12,20 +12,32 @@ app.get("/", (req, res) => {
   //   res.send("Creating_a_Student_info");
   res.sendFile(__dirname + "/index.html");
 });
+// app.post("/register", (req, res) => {
+//   const name = req.body.myname;
+//   const age = req.body.age;
+//   const clas = req.body.clas;
+//   const number = req.body.number;
+//   const email = req.body.email;
+//   const addr = req.body.address;
+//   const dob = req.body.dob;
 app.post("/register", (req, res) => {
-  const name = req.body.myname;
-  const age = req.body.age;
-  const clas = req.body.clas;
-  const number = req.body.number;
-  const email = req.body.email;
-  const addr = req.body.address;
-  const dob = req.body.dob;
-  console.log(
-    `Name: ${name} \nAge: ${age} \nClass: ${clas} \nNumber: ${number} \nEmail: ${email}\nAddress: ${addr}\nDate_of_Birth: ${dob}`
-  );
+  const studData = {
+    naam: req.body.myname,
+    age: req.body.age,
+    clas: req.body.clas,
+    number: req.body.number,
+    email: req.body.email,
+    addr: req.body.address,
+    dob: req.body.dob,
+  };
+  // console.log(
+  //   `Name: ${naam} \nAge: ${age} \nClass: ${clas} \nNumber: ${number} \nEmail: ${email}\nAddress: ${addr}\nDate_of_Birth: ${dob}`
+  // );
+  console.log(studData);
   fs.appendFile(
     "student.txt",
-    `Name: ${name}  Age: ${age}  Class: ${clas}  Number: ${number}  Email: ${email}  Address: ${addr}  Date_of_Birth: ${dob}\n`,
+    `Name: ${req.body.naam}  Age: ${req.body.age}  Class: ${req.body.clas}  Number: ${req.body.number}  Email: ${req.body.email}  Address: ${req.body.addr}  Date_of_Birth: ${req.body.dob}\n`,
+    studData,
     (err) => {
       if (err) {
         console.log(err);
@@ -35,7 +47,7 @@ app.post("/register", (req, res) => {
   res
     .status(201)
     .send(
-      `Name: ${name} <br />Age: ${age} <br />Class: ${clas} <br />Number: ${number} <br />Email: ${email}<br />Address: ${addr}<br />Date_of_Birth: ${dob}`
+      `Name: ${studData.naam} <br />Age: ${studData.age} <br />Class: ${studData.clas} <br />Number: ${studData.number} <br />Email: ${studData.email}<br />Address: ${studData.addr}<br />Date_of_Birth: ${studData.dob}`
     );
 });
 
